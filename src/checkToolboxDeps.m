bde689c61d7bf4362ab0179b9556e389ace62e38
Pawan Pentamaraju <7398963+pawanpe@users.noreply.github.com>
2024-12-18 11:27:10 -0500

    Update checkToolboxDeps.m - for gitlab changes in integb.

--- src/checkToolboxDeps.m
+++ src/checkToolboxDeps.m
@@ -1,6 +1,6 @@
 function checkToolboxDeps
 % CHECKTOOLBOXDEPS used to run at project startup and check for dependent
-% toolboxes.
+% toolboxes. Also print if any toolboxes required.
 
     toolboxes = ["finance"];
     
@@ -8,6 +8,7 @@ function checkToolboxDeps
         a = ver(toolboxes(i));
         if isempty(a)
             fprintf("This project requires the %s toolbox, it doesn't seem to be installed. Please install it.\n", toolboxes(i))
+            doc 'plot'
         end
     end
 
